c2d45d127d6bca64ad1eacd0e844e47972ed93c2
mili-08 <milijmartinez@gmail.com>
2023-08-10 00:32:10 -0300

    Se añadio la redireccion del registro al login

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const app = express();
 
 const PORT = 3030;
 
+
 app.use(express.static("public"));
 
-app.get("/", (req, res)=> {
-    res.sendFile(__dirname + '/views/register.html');
-})
+app.get('/register.html',(req,res)=> {
+    res.sendFile(__dirname + '/views/register.html')
+});
+
+app.get('/login.html', (req,res)=> {
+    res.sendFile(__dirname + '/views/login.html')
+});
 
 app.listen(PORT, () => {
-    console.log(`[Server]: running on port ${PORT}`);
-})
+    console.log(`[Server]: running on port ${PORT}`)
+});
