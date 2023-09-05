e53c6f2bc7c59fba5468c9ae9f0a8a06b280b980
fernandasivila <fernandabsivila@gmail.com>
2023-09-05 11:07:59 -0300

    metodo PUT para poder actualizar un producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const products = require('../data/products.json');
 
 
@@ -13,6 +14,38 @@ const productController = {
     },
     productAdd: (req,res)=> {
         res.render("productAdd");
+    },
+    productProcess:(req,res)=>{
+        const editedProduct={
+            id: req.body.id,
+        name: req.body.name,
+        descripcion: req.body.descripcion,
+        viñedo: req.body.viñedo,
+        edad: req.body.edad,
+        altitud: req.body.altitud,
+        variedad: req.body.variedad,
+        barriles:req.body.barriles,
+        guardado:req.body.guardado,
+        priceUnity:req.body.priceUnity,
+        priceSix:req.body.priceSix,
+        afrutado:req.body.afrutado,
+        nada:req.body.nada,
+        seco:req.body.seco,
+        amable:req.body.amable,
+        aterciopelado:req.body.aterciopelado,
+        liviano:req.body.liviano,
+        delicado:req.body.delicado,
+        img:req.body.img,
+        }
+
+        const idProduct= req.params.id;
+        products.find((p)=> p.id == idProduct? p=editedProduct : null);
+
+        const productsJSON= JSON.stringify(products);
+
+        fs.writeFileSync('../data/products.json', productsJSON);
+
+        res.redirect('/productDetail/:idProduct');
     }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,11 @@ routes.get(['/', '/home'], userController.home);
 routes.get('/register', userController.register);
 routes.get('/login', userController.login);
 routes.get('/carrito', productController.carrito);
+
 routes.get('/productDetail/:id', productController.productoDetalle);
 routes.get('/productAdd', productController.productAdd);
+
+//update
+routes.put("/productDetail/:id", productController.productProcess);
 
 module.exports = routes;
