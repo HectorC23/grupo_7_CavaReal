8b20e255a86d26fee09070083957c58a4c1a53de
mili-08 <milijmartinez@gmail.com>
2023-09-06 20:02:15 -0300

    Algunos cambios

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,7 @@
-const products = require('../data/products.json');
+let products = require('../data/products.json');
+const fs = require('fs');
+const path = require('path');
+let idProduct = 9;
 
 
 const productController = {
@@ -11,8 +14,23 @@ const productController = {
         const product = products.find(p =>p.id == id)
         res.render("productDetail",{product});
     },
-    productAdd: (req,res)=> {
+    productAddView: (req,res)=> {
         res.render("productAdd");
+    },
+    productAdd: (req,res) => {
+     const product = req.body;
+     product.id = idProduct + 1;
+     product.edad = +product.edad;
+     product.altitud = +product.altitud;
+     product.guardado = +product.guardado;
+     product.potencial = +product.potencial;
+     product.precio = +product.precio; // AHI QUE VER QUE ONDA CON LOS CATEGOTIAS PARA SABER SI SON numero;
+     products.push(product);
+     
+     fs.writeFileSync(path.join(__dirname, '../data/products.json'),JSON.stringify(products));
+
+     res.redirect('/home');
+     console.log(product);
     }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,8 @@ routes.get('/register', userController.register);
 routes.get('/login', userController.login);
 routes.get('/carrito', productController.carrito);
 routes.get('/productDetail/:id', productController.productoDetalle);
-routes.get('/productAdd', productController.productAdd);
+routes.get('/productAdd', productController.productAddView);
+
+routes.post('/productAdd',productController.productAdd);
 
 module.exports = routes;
