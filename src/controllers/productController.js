5853044a7cc313900a53f108c5ac9a0b3cb68a40
mili-08 <milijmartinez@gmail.com>
2023-09-09 15:12:48 -0300

    Cambios

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -16,36 +16,36 @@ const productController = {
     },
 
     process:(req,res)=>{
-        const editedProduct={
-            id: req.body.id,
-            name: req.body.name,
-            descripcion: req.body.descripcion,
-            viñedo: req.body.viñedo,
-            edad: req.body.edad,
-            altitud: req.body.altitud,
-            variedad: req.body.variedad,
-            barriles:req.body.barriles,
-            guardado:req.body.guardado,
-            priceUnity:req.body.priceUnity,
-            priceSix:req.body.priceSix,
-            afrutado:req.body.afrutado,
-            nada:req.body.nada,
-            seco:req.body.seco,
-            amable:req.body.amable,
-            aterciopelado:req.body.aterciopelado,
-            liviano:req.body.liviano,
-            delicado:req.body.delicado,
-            img:req.body.img,
-        }
 
-        const idProduct= req.params.id;
-        products.find((p)=> p.id == idProduct? p=editedProduct : null);
+        const idProduct= +req.params.id;
 
-        const productsJSON= JSON.stringify(products);
+        products.forEach(e => {
+            if(e.id == idProduct) {
+                e.id= req.body.id,
+                e.name= req.body.name,
+                e.descripcion= req.body.descripcion,
+                e.viñedo= req.body.viñedo,
+                e.edad= req.body.edad,
+                e.altitud= req.body.altitud,
+                e.variedad= req.body.variedad,
+                e.barriles= req.body.barriles,
+                e.guardado = req.body.guardado,
+                e.priceUnity= req.body.priceUnity,
+                e.priceSix= req.body.priceSix,
+                e.afrutado= req.body.afrutado,
+                e.nada= req.body.nada,
+                e.seco= req.body.seco,
+                e.amable= req.body.amable,
+                e.aterciopelado= req.body.aterciopelado,
+                e.liviano= req.body.liviano,
+                e.delicado=req.body.delicado,
+                e.img=req.body.img 
+            }
+        });
+        console.log(products);
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'),JSON.stringify(products));
 
-        fs.writeFileSync('../data/products.json', productsJSON);
-
-        res.redirect('/product/detail/:idProduct');
+        res.redirect('/home');
     },
     edit: (req, res) => {
         const { id } =req.params;
@@ -83,3 +83,29 @@ const productController = {
 }
 
 module.exports = productController;
+
+
+/*const editedProduct={
+    id: req.body.id,
+    name: req.body.name,
+    descripcion: req.body.descripcion,
+    viñedo: req.body.viñedo,
+    edad: req.body.edad,
+    altitud: req.body.altitud,
+    variedad: req.body.variedad,
+    barriles:req.body.barriles,
+    guardado:req.body.guardado,
+    priceUnity:req.body.priceUnity,
+    priceSix:req.body.priceSix,
+    afrutado:req.body.afrutado,
+    nada:req.body.nada,
+    seco:req.body.seco,
+    amable:req.body.amable,
+    aterciopelado:req.body.aterciopelado,
+    liviano:req.body.liviano,
+    delicado:req.body.delicado,
+    img:req.body.img,
+
+
+    res.redirect('/product/detail/:idProduct');
+}*/
