8cd3b2309de2c6ed136382f0c7ead3fda61ae14e
fernandasivila <fernandabsivila@gmail.com>
2023-11-10 01:00:46 -0300

    Remodelaciones: ProductDetail, ProductAdd, Cart

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -7,6 +7,19 @@ const controller = {
     carrito: (req,res)=> {
         let products = require("../data/products.json");
         res.render("productsList",{products});
+    },
+    detail: (req,res)=> {
+        let products = require("../data/products.json");
+        let product = products[0];
+        res.render("productDetail",{product});
+    },
+    add: (req,res)=> {
+        let products = require("../data/products.json");
+        res.render("productAdd",{products});
+    },
+    cart: (req,res)=> {
+        let products = require("../data/products.json");
+        res.render("shoppingCart",{products});
     }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,9 @@ const indexController = require('../controllers/indexController')
 router.get(['/', '/home'], indexController.home);
 router.get('/carrito', indexController.carrito);
 
+//BORRAR
+router.get('/detail', indexController.detail);
+router.get('/add', indexController.add);
+router.get('/cart', indexController.cart);
 
 module.exports = router;
